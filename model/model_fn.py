690837f57ed1ea38192af201fa41cf50cfc997a5
Christopher Koenig <koenig97@stanford.edu>
2019-06-05 11:25:59 -0700

    Last test.

--- model/model_fn.py
+++ model/model_fn.py
@@ -26,7 +26,7 @@ def build_model(is_training, inputs, params):
     bn_momentum = params.bn_momentum
     dropout_rate = params.dropout_rate
     regularizer = tf.contrib.layers.l2_regularizer(scale=params.regularization_rate)
-    channels = [num_channels, num_channels * 2]
+    channels = [num_channels, num_channels * 2, num_channels * 4, num_channels * 8, num_channels * 8]
     for i, c in enumerate(channels):
         with tf.variable_scope('block_{}'.format(i+1)):
             out = tf.layers.conv2d(out, c, 3, padding='same', kernel_regularizer=regularizer)
@@ -36,17 +36,24 @@ def build_model(is_training, inputs, params):
             out = tf.layers.max_pooling2d(out, 2, 2)
 
     np = int(params.image_size / (2 ** len(channels))) # num "pixels" wide
-    assert out.get_shape().as_list() == [None, np, np, num_channels * 2]
+    assert out.get_shape().as_list() == [None, np, np, num_channels * 8]
 
-    out = tf.reshape(out, [-1, np * np * num_channels * 2])
+    out = tf.reshape(out, [-1, np * np * num_channels * 8])
     with tf.variable_scope('fc_1'):
-        out = tf.layers.dense(out, num_channels * 2, kernel_regularizer=regularizer)
+        out = tf.layers.dense(out, num_channels * 8, kernel_regularizer=regularizer)
         if params.use_batch_norm:
             out = tf.layers.batch_normalization(out, momentum=bn_momentum, training=is_training)
         out = tf.nn.relu(out)
         if params.use_dropout:
             out = tf.layers.dropout(out, rate=dropout_rate, training=is_training)
     with tf.variable_scope('fc_2'):
+        out = tf.layers.dense(out, num_channels * 8, kernel_regularizer=regularizer)
+        if params.use_batch_norm:
+            out = tf.layers.batch_normalization(out, momentum=bn_momentum, training=is_training)
+        out = tf.nn.relu(out)
+        if params.use_dropout:
+            out = tf.layers.dropout(out, rate=dropout_rate, training=is_training)
+    with tf.variable_scope('fc_3'):
         logits = tf.layers.dense(out, params.num_labels, kernel_regularizer=regularizer)
 
     return logits

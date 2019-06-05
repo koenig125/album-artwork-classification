d3db7cd5abda0cf3177ebf4402a6d1ea44e45009
Christopher Koenig <koenig97@stanford.edu>
2019-06-04 21:39:39 -0700

    Fix label generation.

--- build_dataset.py
+++ build_dataset.py
@@ -107,9 +107,6 @@ def generate_labels(filenames, album_genres, output_dir, split):
         genres = album_genres[img_id]
         album_label = [1 if g in genres else 0 for g in genre_list]
         if sum(album_label) != 1: continue
-        if len(files) % 100 == 0:
-            print(genres)
-            print(album_label)
         labels.append(album_label)
         files.append(f)
     output_file = os.path.join(output_dir, 'y_' + split + '.npy')

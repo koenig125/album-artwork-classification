b4132cd10cbf4bd9e84b5566119641b09823fe0e
Christopher Koenig <koenig97@stanford.edu>
2019-06-05 08:49:13 -0700

    Add incorrect image labeling summary.

--- model/model_fn.py
+++ model/model_fn.py
@@ -123,12 +123,9 @@ def model_fn(mode, inputs, params, reuse=False):
 
     # Add incorrectly labeled images
     if mode == 'eval':
-        print(labels)
-        print(predictions)
         mask = tf.not_equal(tf.argmax(labels, 1), predictions)
-        print(mask)
         for label in range(0, params.num_labels):
-            mask_label = tf.logical_and(mask, tf.equal(predictions, tf.cast(tf.one_hot(label, params.num_labels), tf.int64)))
+            mask_label = tf.logical_and(mask, tf.equal(predictions, label))
             incorrect_image_label = tf.boolean_mask(inputs['images'], mask_label)
             tf.summary.image('incorrectly_labeled_{}'.format(label), incorrect_image_label)
 
